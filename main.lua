985b14ab9e529228df5b16b979f039cae676cd7b
Michael Walker <mike@barrucadu.co.uk>
2013-04-27 15:44:58 +0100

    Add moveable player

--- main.lua
+++ main.lua
@@ -1,18 +1,44 @@
 function love.load()
    -- Load game resources (music, images) from files, and set up initial state.
    paused = false
+
+   player = { x = 200, -- Position
+              y = 150,
+              sprite = love.graphics.newImage("player.png"), -- Sprite
+              width = 0,
+              height = 0}
+
+   player.width = player.sprite:getWidth()
+   player.height = player.sprite:getHeight()
 end
 
 function love.update(dt)
    -- Update the game state. dt is the time delta. Don't do anything if paused.
-
    if paused then
       return
+   end
+
+   -- Position
+   if love.keyboard.isDown("right") then
+      player.x = player.x + dt * 100
+   elseif love.keyboard.isDown("left") then
+      player.x = player.x - dt * 100
+   end
+
+   if love.keyboard.isDown("up") then
+      player.y = player.y - dt * 100
+   elseif love.keyboard.isDown("down") then
+      player.y = player.y + dt * 100
    end
 end
 
 function love.draw()
    -- Draw a frame
+   if paused then
+      love.graphics.print("Paused", 0, 0)
+   end
+
+   love.graphics.draw(player.sprite, player.x, player.y, 0, 1, 1, player.width / 2, player.height / 2)
 end
 
 function love.mousepressed(x, y, button)
