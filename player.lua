c8008c601c9a4da363a8a25f9bb2be29e25dce9b
Michael Walker <mike@barrucadu.co.uk>
2013-04-27 23:45:45 +0100

    Change text colour of stats to black

--- player.lua
+++ player.lua
@@ -43,11 +43,16 @@ function Player:update(dt)
 end
 
 function Player:draw()
+   r, g, b, a = love.graphics.getColor()
+   love.graphics.setColor(0, 0, 0, 255)
+
    love.graphics.print("Score:  " .. self.score, 10, 10)
    love.graphics.print("Power:  " .. self.power, 10, 25)
    love.graphics.print("Bombs:  " .. self.bombs, 10, 40)
    love.graphics.print("Lives:  " .. self.lives, 10, 55)
    love.graphics.print("Health: " .. self.health .. " / " .. self.maxhealth, 10, 70)
+
+   love.graphics.setColor(r, g, b, a)
 
    self.super.draw(self)
 end
