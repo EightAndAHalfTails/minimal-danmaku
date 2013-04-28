e69a47f92173cff3c3b340e08100d4a4bfb0ace0
EightAndAHalfTails <jakebhumphrey@gmail.com>
2013-04-28 16:39:36 +0100

    fixed enemy type representation in scripts

--- stage1.lua
+++ stage1.lua
@@ -7,11 +7,11 @@ local time = 0.0
 
 -- Timings are in deciseconds
 Script = {
-   [4] = { { enemy = "basic", x = 100, y = 0 },
-	   { enemy = "basic", x = 400, y = 0 }, done = false },
-   [8] = { { enemy = "basic", x = 200, y = 0 },
-	   { enemy = "basic", x = 300, y = 0 }, done = false },
-   [40] = { { enemy = "basic", x = 250, y = 0}, done = false }
+   [4] = { { enemyType = enemy.BasicEnemy, x = 100, y = 0 },
+	   { enemyType = enemy.BasicEnemy, x = 400, y = 0 }, done = false },
+   [8] = { { enemyType = enemy.BasicEnemy, x = 200, y = 0 },
+	   { enemyType = enemy.BasicEnemy, x = 300, y = 0 }, done = false },
+   [40] = { { enemyType = enemy.BasicEnemy, x = 250, y = 0}, done = false }
 }
 
 function update(dt)
@@ -21,6 +21,7 @@ function update(dt)
       if t < time then
 	 if not list.done then
 	    for i, e in ipairs(list) do
+	       print(e.enemyType)
 	       execute(e)
 	    list.done = true
 	    end
@@ -30,8 +31,8 @@ function update(dt)
 end
 
 function execute(entry)
-   if entry.enemy == "basic" then
-      new_enemy = enemy.BasicEnemy:create()
+   if entry.enemyType then
+      new_enemy = entry.enemyType:create()
       new_enemy:initialise(entry.x, entry.y)
       table.insert(globals.enemies, new_enemy)
    end
